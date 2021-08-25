ed263d4f1905436d91906d4b1d0d3c9295e3936d
Guilherme Anselmi <60523666+GuilhermeAnselmi@users.noreply.github.com>
2021-08-24 23:36:56 -0300

    Adicionar arquivos de projeto.

---
+++ Quantity/Quantity/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Quantity
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Sub(object sender, EventArgs e)
+        {
+            if (int.Parse(lblNumber.Text) > 0)
+            {
+                lblNumber.Text = (int.Parse(lblNumber.Text) - 1).ToString();
+            }
+        }
+
+        private void Soma(object sender, EventArgs e)
+        {
+            lblNumber.Text = (int.Parse(lblNumber.Text) + 1).ToString();
+        }
+    }
+}
